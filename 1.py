23b4fe698882dab7cd80d86afc971eade5a02a42
Todvaa <todvaa@gmail.com>
2022-07-14 12:55:29 +0300

    Init commit

---
+++ 1.py
@@ -0,0 +1,15 @@
+import json
+
+import requests
+import pandas as pd
+from pylab import *
+
+base_link = "https://api.binance.com"
+path_link = "/api/v3/klines"
+url = base_link + path_link
+param = {'symbol': 'BTCUSDC', 'interval': '1d', 'limit': 1000}
+
+quotes = requests.get(url, params=param)
+data = ''
+dec_data = json.loads(quotes.text)
+print(dec_data)
